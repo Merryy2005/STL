24788e1198ebe86911d4a950c784b6a1e629a88a
Merygithub <meri.badalyan2005@gmail.com>
2024-12-07 17:11:40 +0400

    find_if_not

---
+++ Algorithms/find_if_not.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+namespace mystl
+{
+    template<typename InputIt, typename UnaryPred>
+    InputIt find_if_not(InputIt first, InputIt last, UnaryPred p)
+    {
+        for(;first != last ; ++first)
+        {
+            if(!p(*first)) return first;
+        }
+        return last;
+    }
+}
+
+int main()
+{
+    std::vector<int> v{1,2,3,4,5};
+    auto it = mystl::find_if_not(v.begin(), v.end(), [](const int& el) -> bool {return el < 4;});
+    if(it != v.end())
+    {
+        std::cout << "Found first element at pos " << (it - v.begin()) << std::endl;
+    }
+    else
+    {
+        std::cout << "None" << std::endl;
+    }
+    return 0;
+}
